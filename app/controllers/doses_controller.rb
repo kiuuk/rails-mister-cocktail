bc90db317c49c091e266a60ac08adef9d20017d8
Kay Ha <kiwook.ha@gmail.com>
2018-11-09 12:35:12 +0900

    done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,15 +1,23 @@
 class DosesController < ApplicationController
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new(cocktail: @cocktail)
+    @dose = Dose.new
   end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      # raise
+      render :new
+
+    end
   end
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
